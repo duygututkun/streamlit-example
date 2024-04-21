baa55c97587d3689c2712dee3e360a573a4cada7
duygututkun <167673075+duygututkun@users.noreply.github.com>
2024-04-21 10:55:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,49 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
+(.venv) ogrencigirisi@Mac-MacBook-Pro-2 trendyorum % pip freeze requirements
+altair==5.3.0
+attrs==23.2.0
+blinker==1.7.0
+cachetools==5.3.3
+certifi==2024.2.2
+charset-normalizer==3.3.2
+click==8.1.7
+gitdb==4.0.11
+GitPython==3.1.43
+idna==3.7
+Jinja2==3.1.3
+joblib==1.4.0
+jsonschema==4.21.1
+jsonschema-specifications==2023.12.1
+markdown-it-py==3.0.0
+MarkupSafe==2.1.5
+mdurl==0.1.2
+numpy==1.26.4
+packaging==24.0
+pandas==2.2.2
+pillow==10.3.0
+protobuf==4.25.3
+pyarrow==16.0.0
+pydeck==0.8.1b0
+Pygments==2.17.2
+python-dateutil==2.9.0.post0
+pytz==2024.1
+referencing==0.34.0
+requests==2.31.0
+rich==13.7.1
+rpds-py==0.18.0
+scikit-learn==1.4.2
+scipy==1.13.0
+six==1.16.0
+smmap==5.0.1
+streamlit==1.33.0
+tenacity==8.2.3
+threadpoolctl==3.4.0
+toml==0.10.2
+toolz==0.12.1
+tornado==6.4
+typing_extensions==4.11.0
+tzdata==2024.1
+urllib3==2.2.1
+(.venv) ogrencigirisi@Mac-MacBook-Pro-2 trendyorum % pip freeze >requirements.txt
+(.venv) ogrencigirisi@Mac-MacBook-Pro-2 trendyorum % 
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
